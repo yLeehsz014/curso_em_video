0a9eef30f939da5c2c89f908a39dc3a38e46dfca
Lethicia Almeida <lethiciaa014@gmail.com>
2024-04-29 23:30:16 -0300

    day2 - challenge

--- 100dayscss/day2/script.js
+++ 100dayscss/day2/script.js
@@ -1,12 +1,4 @@
-const box = document.querySelector('.box-lines')
-const line1 = document.getElementById('line1');
-const line2 = document.getElementById('line2');
-const line3 = document.getElementById('line3');
+const box = document.querySelector('.box-lines');
 box.addEventListener('click',()=>{
-    setTimeout(function() { 
-        line2.classList.toggle('line-off')
-        line1.classList.toggle('line1-rotate')
-        line3.classList.toggle('line3-rotate')
-    }, 100);
-    
+    box.classList.toggle('active');
 })
